d41b2e500f46b77a10eb61417fac6c33757d0af3
Yassine Zeriouh <yassine@zeriouh.io>
2021-05-24 14:48:34 +0200

    feat(product): create model

---
+++ src/app/product/product.model.ts
@@ -0,0 +1,32 @@
+import { Category } from './category.enum';
+
+// Since I am not using any store to query data
+// I thought using a class as a model is a better approach
+// as I can query data with instance methods
+export class Product {
+  id!: string;
+  name!: string;
+  manufacturer!: string;
+  categories?: Category[];
+  priceCents!: number;
+  onSale?: boolean;
+  // This comes with problems when scaling since we may need translations.
+  // Also, the whole class could be abstracted so every
+  // product type would be extended by the abstract product class,
+  // so it has its own props (color, battery durability, inside material etc.)
+  // However, I think this solution is better in this case since its easier to introduce new products
+  description?: string;
+
+  constructor(v: Product | Partial<Product> = {}) {
+    Object.assign(this, v);
+    // Not necessary since it can be caught with form validation
+    // though a good example on how to handle optional/default values in a class model
+    this.categories = v.categories ?? [];
+    this.description = v.description ?? '';
+    this.onSale = v.onSale ?? false;
+  }
+
+  get price(): number {
+    return this.priceCents / 100;
+  }
+}
