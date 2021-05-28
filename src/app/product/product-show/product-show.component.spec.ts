dd77567e46eea3e2eb836ffb3a560d5075b46972
Yassine Zeriouh <yassine@zeriouh.io>
2021-05-28 06:21:09 +0200

    feat(product-show): implement show view

--- src/app/product/product-show/product-show.component.spec.ts
+++ src/app/product/product-show/product-show.component.spec.ts
@@ -1,25 +1,61 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductShowComponent } from './product-show.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { Product } from '../product.model';
+import { Category } from '../category.enum';
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
 
 describe('ProductShowComponent', () => {
   let component: ProductShowComponent;
   let fixture: ComponentFixture<ProductShowComponent>;
 
+  const product = new Product({
+    id: '67PT5W-E12TT',
+    name: 'Uhr',
+    manufacturer: 'SportsInc',
+    categories: [
+      Category.SPORT,
+      Category.WATCH
+    ],
+    priceCents: 11000,
+    description: `Smartwatch\nMaterial: Kunststoff\nAkku-Laufzeit: bis zu 7 Tage\nFarbe: Weiss\nGewicht: 77g\nSonstiges:\nwasserdicht, Sicherheitsglas`,
+    imageUrl: 'assets/watch.jpg'
+  });
+
   beforeEach(async () => {
+    registerLocaleData(localeDe);
     await TestBed.configureTestingModule({
-      declarations: [ ProductShowComponent ]
-    })
-    .compileComponents();
+      declarations: [ProductShowComponent],
+      imports: [RouterTestingModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            data: of({ product })
+          }
+        }
+      ]
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductShowComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit', () => {
+    it('should load product from route data', () => {
+      expect(component.product).toBeUndefined();
+      fixture.detectChanges();
+      expect(component.product).toEqual(product);
+    });
   });
 });
--- src/app/product/product-show/product-show.component.ts
+++ src/app/product/product-show/product-show.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { first, map } from 'rxjs/operators';
+import { Product } from '../product.model';
+import { capitalizeFirstLetter } from '../../utils/capitalize-first-letter';
 
 @Component({
   selector: 'app-product-show',
@@ -7,10 +11,16 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.Emulated
 })
 export class ProductShowComponent implements OnInit {
+  product?: Product;
+  capitalizeFirstLetter = capitalizeFirstLetter;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute) {
   }
 
+  ngOnInit(): void {
+    this.route.data.pipe(
+      first(),
+      map(data => data.product)
+    ).subscribe(product => this.product = product);
+  }
 }
