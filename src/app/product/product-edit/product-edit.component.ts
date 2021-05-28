618eb5eb72a3e9363c37f0f54a63d66ae6ad8965
Yassine Zeriouh <yassine@zeriouh.io>
2021-05-28 03:25:40 +0200

    feat(product): scaffold product-edit

---
+++ src/app/product/product-edit/product-edit.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-product-edit',
+  templateUrl: './product-edit.component.html',
+  styleUrls: ['./product-edit.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class ProductEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
