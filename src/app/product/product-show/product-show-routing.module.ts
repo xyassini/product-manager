e8ad0282530410deb6dbc42112ec8cf098557329
Yassine Zeriouh <yassine@zeriouh.io>
2021-05-28 05:12:38 +0200

    feat(product-show): add routes * base route, edit, delete

--- src/app/product/product-show/product-show-routing.module.ts
+++ src/app/product/product-show/product-show-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductShowComponent } from './product-show.component';
 
-const routes: Routes = [];
+
+// Edit and delete routes could be moved to AppRoutingModule
+// since these routes are also accessible from ProductListComponent,
+// which is loaded in the AppModule.
+// e.g.: click on edit in ProductList -> Lazy-Load ProductShowModule -> Lazy-Load ProductEditModule
+// instead of click on edit -> Lazy-Load ProductEditModule
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductShowComponent
+  },
+  {
+    path: 'edit',
+    loadChildren: () => import('../product-edit/product-edit.module').then(m => m.ProductEditModule)
+  },
+  {
+    path: 'delete',
+    loadChildren: () => import('../product-delete/product-delete.module').then(m => m.ProductDeleteModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
